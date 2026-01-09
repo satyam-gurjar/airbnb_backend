b62288843fe829469d2dc90a3789d2fab5920b86
satya gurjar <satyagurjar405@gmail.com>
2026-01-09 20:05:15 +0530

    learn how to make public any dir and file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Core module
-const path = require('path')
+const path = require('path');
 
 //External Module
 const express = require('express');
@@ -17,10 +17,11 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host",hostRouter);
 
+app.use(express.static(path.join(rootDir,'public')));
 
 app.use((req,res,next) => {
 
-  res.status(404).sendFile(path.join(rootDir, 'views','404.html'))
+  res.status(404).sendFile(path.join(rootDir, 'views','404.html'));
 })
 
 
